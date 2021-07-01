f66710617b46f89479d1d08be7a9ae3a9bc78d27
Kiritow <1362050620@qq.com>
2021-07-01 19:54:03 +0800

    Add event handler

--- app.js
+++ app.js
@@ -9,5 +9,39 @@ app.use(koaBodyParser());
 app.use(koaJson());
 const router = new koaRouter();
 
+function GrantAccess(ctx) {
+    ctx.body = {
+        "reject": false,
+        "unchange": true
+    }
+}
+
+function RejectAccess(ctx, reason) {
+    ctx.body = {
+        "reject": true,
+        "reject_reason": reason || "Access denied.",
+    }
+}
+
+router.post('/event/NewProxy', async (ctx) => {
+    const postData = ctx.request.body;
+    const {
+        proxy_name: proxyName,
+        proxy_type: proxyType,
+        remote_port: remotePort,
+        metas: metadata,
+    } = postData.content;
+
+    if (proxyType !== 'tcp' || !proxyName.startsWith('container-service-') ) {
+        return GrantAccess(ctx);
+    }
+
+    if (!metadata || !metadata.domain) {
+        return RejectAccess(ctx, 'no domain provided.');
+    }
+
+    return GrantAccess(ctx);
+});
+
 app.use(router.routes()).use(router.allowedMethods());
 app.listen(1234);
